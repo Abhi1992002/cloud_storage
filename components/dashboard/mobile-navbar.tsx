1f597b769db5043b8e3a9d9e4fae137c1f1e33de
abhi1992002 <abhimanyu1992002@gmail.com>
2024-02-15 21:10:23 +0530

    .

--- components/dashboard/mobile-navbar.tsx
+++ components/dashboard/mobile-navbar.tsx
@@ -10,12 +10,13 @@ import {
 } from "@/components/ui/sheet";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { MobileSidebar } from "./mobile-sidebar";
+import { UserButton } from "../auth/user-button";
 
 type MobileNavbarProps = {};
 
 export const MobileNavbar = ({}: MobileNavbarProps) => {
   return (
-    <div className="w-full">
+    <div className="w-full flex justify-between">
       <Sheet>
         <SheetTrigger>
           <HamburgerMenuIcon />
@@ -28,6 +29,7 @@ export const MobileNavbar = ({}: MobileNavbarProps) => {
           </SheetHeader>
         </SheetContent>
       </Sheet>
+      <UserButton />
     </div>
   );
 };
